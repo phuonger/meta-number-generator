eeac8c52549e3b892e9cae546b8ad13d2398b21e
phuonger <60527753+phuonger@users.noreply.github.com>
2026-05-03 22:38:32 +0200

    Update main.cjs

--- electron/main.cjs
+++ electron/main.cjs
@@ -9,14 +9,14 @@ function createWindow() {
     height: 800,
     minWidth: 800,
     minHeight: 600,
-    titleBarStyle: "hiddenInset",
+    titleBarStyle: "hidden",
+    trafficLightPosition: { x: 16, y: 16 },
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
 
-  // Load the built web app
   const indexPath = path.join(__dirname, "..", "dist", "public", "index.html");
   mainWindow.loadFile(indexPath);
 
